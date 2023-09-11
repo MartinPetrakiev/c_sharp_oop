35548443d2f6d23472643b79e34f3e24ea2793f6
martinmp <martomsl@gmail.com>
2023-09-11 16:09:14 +0300

    shapes abstract training

---
+++ Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[]
+            {
+                new Triangle(4, 5),
+                new Rectangle(6, 7),
+                new Square(8)
+            };
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine($"Surface Area {shape.GetType().Name}: {shape.CalculateSurface()} square units");
+            }
+        }
+    }
+}
