cf181c962c8d5e7bbac44278dc8d3ad24cd07e41
martinmp <martomsl@gmail.com>
2023-09-13 11:39:34 +0300

    Common Type System - Student

---
+++ Common_type_system/Enum/Enumerators.cs
@@ -0,0 +1,29 @@
+﻿namespace Common_type_system.Enum
+{
+    public enum Specialty
+    {
+        ComputerScience,
+        Mathematics,
+        Physics,
+        Chemistry,
+        Biology
+    }
+
+    public enum University
+    {
+        Harvard,
+        MIT,
+        Stanford,
+        Yale,
+        Princeton
+    }
+
+    public enum Faculty
+    {
+        Engineering,
+        Science,
+        Arts,
+        Business,
+        Medicine
+    }
+}
